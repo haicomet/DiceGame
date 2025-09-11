887193465e3dd4355db4cd9c60fb3fe356c385b3
hailia sommerville <hailiassommerville@gmail.com>
2025-09-11 13:49:26 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*Ludi : you can play with 2 to 4 players. a player needs to roll two 6s to get a piece out of the base.
+ once a piece is out of the base, the player can move it forward by the number rolled on the dice.
+ if a player rolls a 6, they get another turn. if a player lands on a space occupied by another player's piece, 
+ the other player's piece is sent back to their base. the first player to get all their pieces to the finish line wins the game.
+*/
+
+// |class Dice                
+// --------------------------------
+// - sides : int
+// - topSide : int
+// --------------------------------
+// + Dice()
+// + Dice(int)
+// + Roll() : void
+// + TopSide : int
+// + Sides : int
+// --------------------------------
